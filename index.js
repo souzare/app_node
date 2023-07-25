e20d48753d219e31b6cdb544f8f05c75b6b679dd
Doctor <renansouza.ds@gmail.com>
2023-07-25 18:19:32 -0300

    merge

--- index.js
+++ index.js
@@ -7,14 +7,8 @@ var app = express();
 const client = require('prom-client');
 const counter = new client.Counter({
     name: 'aula_request_total',
-<<<<<<< HEAD
     help: 'Contador de requests'
 });
-=======
-    help: 'Contador de requests',
-    labelNames: ['statusCode']
-})
->>>>>>> 475214911ce21a5c69f733fa06b35826fd4a5e98
 
 app.get('/', function(req, res){
     counter.labels('200').inc();
