690d18e71d3cd12e568c23d4ea9d2109bb9bc449
Doctor <renansouza.ds@gmail.com>
2023-07-30 14:21:21 -0300

    Adicionando as metricas

--- index.js
+++ index.js
@@ -16,10 +16,17 @@ const gauge = new prom.Gauge({
     help: 'Exemplo de gauge'
 });
 
+const histogram = new prom.Histogram({
+    name: 'aula_request_time_seconds',
+    help: 'Tempo de resposta da API',
+    buckets: [0.1, 0.2, 0.3, 0.4, 0.5]
+});
+
 app.get('/', function(req, res){
     counter.labels('200').inc();
     counter.labels('300').inc();
     gauge.set(100*Math.random());
+    histogram.observe(Math.random());
 
 
     res.send('Helo World');
